dec493eb154d66c4ce18619a43a776d540e57866
Odion Sonny-Egbeahie <99491084+Odion-Sonny@users.noreply.github.com>
2023-09-27 19:51:24 +0100

    update

--- update_leaderboard.py
+++ update_leaderboard.py
@@ -3,15 +3,12 @@
 import requests
 from collections import defaultdict
 import os
-# from decouple import config
 
 
 # Define your GitHub repository and authentication token
 repository_owner = "Odion-Sonny"
 repository_name = "tech-feed"
 api_token = os.environ.get("API_TOKEN")
-# api_token = config('API_TOKEN')
-# api_token = ""
 
 # Define the GitHub API endpoint for pull requests
 api_url = f"https://api.github.com/repos/{repository_owner}/{repository_name}/pulls"
